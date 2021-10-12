2fbfbb61b6b8554959f60cdfbbfa6161f04bea1c
Tim Turvey <tturvey@hotmail.co.uk>
2021-10-12 15:49:33 +0100

    Viewing bookmakrs

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/bookmark'
+
 class BookmarkManager < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
@@ -11,12 +13,8 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks' do
-    bookmarks = [
-        "http://www.google.com",
-        "http://www.makersacademy.com"
-    ]
+    @bookmarks = Bookmark.all
     erb :'bookmarks/index'
-
   end
 
   run! if app_file == $0
